db274bc4ae812e14ed2f0cffcb7bbf9d492e03e7
CarolineG85 <caroline.gallix85@gmail.com>
2023-10-10 11:47:35 +0200

    PokemonCard dynamic and Mew has ???

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,13 +1,27 @@
 
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
 
+  },
+];
 
 function PokemonCard() {
+    const pokemon = pokemonList[0]
+    
+    
     return (
-      <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"></img>
-        <figcaption>Bulbasaur</figcaption>
-      </figure>
+    <figure>
+      {pokemon.imgSrc !== undefined ? <img src={pokemon.imgSrc}></img>  :  <p>???</p>} 
+    <figcaption>{pokemon.name}</figcaption>
+    </figure>
     );
-  }
-  
+}
   export default PokemonCard;
+
+  
